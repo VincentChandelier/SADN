99bc1ab798c46ab75d1f3f9143311e265c9424eb
VincentChandelier <49064364+VincentChandelier@users.noreply.github.com>
2022-03-10 16:19:43 +0800

    Add files via upload

---
+++ Code/Network.py
@@ -0,0 +1,463 @@
+
+import math
+import warnings
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from typing import Any
+from thop import profile
+from ptflops import get_model_complexity_info
+from compressai.layers.layers import (
+    AttentionBlock,
+    ResidualBlock,
+    ResidualBlockUpsample,
+    ResidualBlockWithStride,
+    conv3x3,
+    subpel_conv3x3,
+    conv1x1,
+)
+from compressai.entropy_models import GaussianConditional
+from compressai.ans import BufferedRansEncoder, RansDecoder
+
+from compressai.models.utils import update_registered_buffers
+
+LATENT_Depth = 64
+
+from compressai.layers import MaskedConv2d
+from compressai.models.priors import CompressionModel
+
+from compressai.models.priors import JointAutoregressiveHierarchicalPriors
+from compressai.models.waseda import Cheng2020Attention
+
+class InterNet(nn.Module):
+    def __init__(self, channels, angRes, n_blocks, n_layers, analysis=True, *args, **kwargs):
+        super(InterNet, self).__init__(*args, **kwargs)
+        # Feature Extraction
+        if analysis:
+            self.AngFE = nn.Sequential(
+                nn.Conv2d(3, channels, kernel_size=int(angRes), stride=int(angRes), padding=0, bias=False))
+            self.SpaFE = nn.Sequential(
+                nn.Conv2d(3, channels, kernel_size=3, stride=1, dilation=int(angRes), padding=int(angRes), bias=False))
+        else:
+            self.AngFE = nn.Sequential(
+                nn.Conv2d(channels, channels, kernel_size=int(angRes), stride=int(angRes), padding=0, bias=False))
+            self.SpaFE = nn.Sequential(
+                nn.Conv2d(channels, channels, kernel_size=3, stride=1, dilation=int(angRes), padding=int(angRes), bias=False))
+        # Spatial-Angular Interaction
+        self.CascadeInterBlock = CascadeInterBlock(angRes, n_blocks, n_layers, channels)
+        # Fusion and Reconstruction
+        self.BottleNeck = BottleNeck(angRes, n_blocks, channels)
+
+    def forward(self, x):
+        xa = self.AngFE(x)
+        xs = self.SpaFE(x)
+        buffer_a, buffer_s = self.CascadeInterBlock(xa, xs)
+        buffer_out = self.BottleNeck(buffer_a, buffer_s) + xs
+        return buffer_out
+
+
+class make_chains(nn.Module):
+    def __init__(self, angRes, channels):
+        super(make_chains, self).__init__()
+
+        self.Spa2Ang = nn.Conv2d(channels, channels, kernel_size=int(angRes), stride=int(angRes), padding=0, bias=False)
+        self.Ang2Spa = nn.Sequential(
+            nn.Conv2d(channels, int(angRes*angRes*channels), kernel_size=1, stride=1, padding=0, bias=False),
+            nn.PixelShuffle(angRes),
+        )
+        self.AngConvSq = nn.Conv2d(2*channels, channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.SpaConvSq = nn.Conv2d(2*channels, channels, kernel_size=3, stride=1, dilation=int(angRes),
+                                            padding=int(angRes), bias=False)
+        self.ReLU = nn.ReLU(inplace=True)
+
+    def forward(self, xa, xs):
+        buffer_ang1 = xa
+        buffer_ang2 = self.ReLU(self.Spa2Ang(xs))
+        buffer_spa1 = xs
+        buffer_spa2 = self.Ang2Spa(xa)
+        buffer_a = torch.cat((buffer_ang1, buffer_ang2), 1)
+        buffer_s = torch.cat((buffer_spa1, buffer_spa2), 1)
+        out_a = self.ReLU(self.AngConvSq(buffer_a)) + xa
+        out_s = self.ReLU(self.SpaConvSq(buffer_s)) + xs
+        return out_a, out_s
+
+
+class InterBlock(nn.Module):
+    def __init__(self, angRes, n_layers, channels):
+        super(InterBlock, self).__init__()
+        modules = []
+        self.n_layers = n_layers
+        for i in range(n_layers):
+            modules.append(make_chains(angRes, channels))
+        self.chained_layers = nn.Sequential(*modules)
+
+    def forward(self, xa, xs):
+        buffer_a = xa
+        buffer_s = xs
+        for i in range(self.n_layers):
+            buffer_a, buffer_s = self.chained_layers[i](buffer_a, buffer_s)
+        out_a = buffer_a
+        out_s = buffer_s
+        return out_a, out_s
+
+
+class CascadeInterBlock(nn.Module):
+    def __init__(self, angRes, n_blocks, n_layers, channels):
+        super(CascadeInterBlock, self).__init__()
+        self.n_blocks = n_blocks
+        body = []
+        for i in range(n_blocks):
+            body.append(InterBlock(angRes, n_layers, channels))
+        self.body = nn.Sequential(*body)
+    def forward(self, buffer_a, buffer_s):
+        out_a = []
+        out_s = []
+        for i in range(self.n_blocks):
+            buffer_a, buffer_s = self.body[i](buffer_a, buffer_s)
+            out_a.append(buffer_a)
+            out_s.append(buffer_s)
+        return torch.cat(out_a, 1), torch.cat(out_s, 1)
+
+
+class BottleNeck(nn.Module):
+    def __init__(self, angRes, n_blocks, channels):
+        super(BottleNeck, self).__init__()
+
+        self.AngBottle = nn.Conv2d(n_blocks*channels, channels, kernel_size=1, stride=1, padding=0, bias=False)
+        self.Ang2Spa = nn.Sequential(
+            nn.Conv2d(channels, int(angRes * angRes * channels), kernel_size=1, stride=1, padding=0, bias=False),
+            nn.PixelShuffle(angRes),
+        )
+        self.SpaBottle = nn.Conv2d((n_blocks+1)*channels, channels, kernel_size=3, stride=1, dilation=int(angRes),
+                                    padding=int(angRes), bias=False)
+        self.ReLU = nn.ReLU(inplace=True)
+
+    def forward(self, xa, xs):
+        xa = self.ReLU(self.AngBottle(xa))
+        xs = torch.cat((xs, self.Ang2Spa(xa)), 1)
+        out = self.ReLU(self.SpaBottle(xs))
+        return out
+
+SCALES_MIN = 0.11
+SCALES_MAX = 256
+SCALES_LEVELS = 64
+
+
+def get_scale_table(min=SCALES_MIN, max=SCALES_MAX, levels=SCALES_LEVELS):
+    return torch.exp(torch.linspace(math.log(min), math.log(max), levels))
+
+
+class LFContext(CompressionModel):
+    """
+    Args:
+        N (int): Number of channels
+    """
+
+    def __init__(self, N=48, M=LATENT_Depth, angRes=13, n_blocks=4, n_layers=1, **kwargs):
+        super().__init__(entropy_bottleneck_channels=N, **kwargs)
+
+        self.g_a = nn.Sequential(
+            InterNet(N, angRes, n_blocks, n_layers),
+            ResidualBlockWithStride(N, N, stride=2),
+            ResidualBlock(N, N),
+            ResidualBlockWithStride(N, N, stride=2),
+            AttentionBlock(N),
+            ResidualBlock(N, N),
+            ResidualBlockWithStride(N, N, stride=2),
+            ResidualBlock(N, N),
+            conv3x3(N, N, stride=2),
+            AttentionBlock(N),
+            conv3x3(N, M),
+        )
+
+        self.h_a = nn.Sequential(
+            conv3x3(M, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N, stride=2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N, stride=2),
+            nn.LeakyReLU(inplace=True),
+        )
+
+        self.h_s = nn.Sequential(
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            subpel_conv3x3(N, N, 2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N * 3 // 2),
+            nn.LeakyReLU(inplace=True),
+            subpel_conv3x3(N * 3 // 2, N * 3 // 2, 2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N * 3 // 2, M),
+            nn.LeakyReLU(inplace=True),
+        )
+
+        self.g_s = nn.Sequential(
+            conv3x3(M, N),
+            AttentionBlock(N),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            AttentionBlock(N),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            subpel_conv3x3(N, N, 2),
+            InterNet(N, angRes, n_blocks, n_layers, analysis=False),
+            conv3x3(N, 3),
+        )
+
+        self.entropy_parameters = nn.Sequential(
+            nn.Conv2d(M * 2, 640, 1),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(640, 640, 1),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(640, M * 2, 1),
+        )
+
+        self.context_prediction = MaskedConv2d(
+            M, M, kernel_size=5, padding=2, stride=1
+        )
+
+        self.gaussian_conditional = GaussianConditional(None)
+        self.N = int(N)
+        self.M = int(M)
+
+    @property
+    def downsampling_factor(self) -> int:
+        return 2 ** (4 + 2)
+
+    def load_state_dict(self, state_dict):
+        update_registered_buffers(
+            self.gaussian_conditional,
+            "gaussian_conditional",
+            ["_quantized_cdf", "_offset", "_cdf_length", "scale_table"],
+            state_dict,
+        )
+        super().load_state_dict(state_dict)
+
+    @classmethod
+    def from_state_dict(cls, state_dict):
+        """Return a new model instance from `state_dict`."""
+        # N = state_dict["g_a.1.conv1.weight"].size(0)
+        # M = LATENT_Depth
+        # net = cls(N, M)
+        net = cls()
+        net.load_state_dict(state_dict)
+        return net
+
+    def update(self, scale_table=None, force=False):
+        if scale_table is None:
+            scale_table = get_scale_table()
+        updated = self.gaussian_conditional.update_scale_table(scale_table, force=force)
+        updated |= super().update(force=force)
+        return updated
+
+    def forward(self, x):
+        y = self.g_a(x)
+        z = self.h_a(y)
+        z_hat, z_likelihoods = self.entropy_bottleneck(z)
+        params = self.h_s(z_hat)
+
+        y_hat = self.gaussian_conditional.quantize(
+            y, "noise" if self.training else "dequantize"
+        )
+        ctx_params = self.context_prediction(y_hat)
+        gaussian_params = self.entropy_parameters(
+            torch.cat((params, ctx_params), dim=1)
+        )
+        scales_hat, means_hat = gaussian_params.chunk(2, 1)
+        _, y_likelihoods = self.gaussian_conditional(y, scales_hat, means=means_hat)
+        x_hat = self.g_s(y_hat)
+
+        return {
+            "x_hat": x_hat,
+            "likelihoods": {"y": y_likelihoods, "z": z_likelihoods},
+        }
+
+    def compress(self, x):
+        if next(self.parameters()).device != torch.device("cpu"):
+            warnings.warn(
+                "Inference on GPU is not recommended for the autoregressive "
+                "models (the entropy coder is run sequentially on CPU)."
+            )
+
+        y = self.g_a(x)
+        z = self.h_a(y)
+
+        z_strings = self.entropy_bottleneck.compress(z)
+        z_hat = self.entropy_bottleneck.decompress(z_strings, z.size()[-2:])
+
+        params = self.h_s(z_hat)
+
+        s = 4  # scaling factor between z and y
+        kernel_size = 5  # context prediction kernel size
+        padding = (kernel_size - 1) // 2
+
+        y_height = z_hat.size(2) * s
+        y_width = z_hat.size(3) * s
+
+        y_hat = F.pad(y, (padding, padding, padding, padding))
+
+        y_strings = []
+        for i in range(y.size(0)):
+            string = self._compress_ar(
+                y_hat[i : i + 1],
+                params[i : i + 1],
+                y_height,
+                y_width,
+                kernel_size,
+                padding,
+            )
+            y_strings.append(string)
+
+        return {"strings": [y_strings, z_strings], "shape": z.size()[-2:]}
+
+    def _compress_ar(self, y_hat, params, height, width, kernel_size, padding):
+        cdf = self.gaussian_conditional.quantized_cdf.tolist()
+        cdf_lengths = self.gaussian_conditional.cdf_length.tolist()
+        offsets = self.gaussian_conditional.offset.tolist()
+
+        encoder = BufferedRansEncoder()
+        symbols_list = []
+        indexes_list = []
+
+        # Warning, this is slow...
+        # TODO: profile the calls to the bindings...
+        masked_weight = self.context_prediction.weight * self.context_prediction.mask
+        for h in range(height):
+            for w in range(width):
+                y_crop = y_hat[:, :, h : h + kernel_size, w : w + kernel_size]
+                ctx_p = F.conv2d(
+                    y_crop,
+                    masked_weight,
+                    bias=self.context_prediction.bias,
+                )
+
+                # 1x1 conv for the entropy parameters prediction network, so
+                # we only keep the elements in the "center"
+                p = params[:, :, h : h + 1, w : w + 1]
+                gaussian_params = self.entropy_parameters(torch.cat((p, ctx_p), dim=1))
+                gaussian_params = gaussian_params.squeeze(3).squeeze(2)
+                scales_hat, means_hat = gaussian_params.chunk(2, 1)
+
+                indexes = self.gaussian_conditional.build_indexes(scales_hat)
+
+                y_crop = y_crop[:, :, padding, padding]
+                y_q = self.gaussian_conditional.quantize(y_crop, "symbols", means_hat)
+                y_hat[:, :, h + padding, w + padding] = y_q + means_hat
+
+                symbols_list.extend(y_q.squeeze().tolist())
+                indexes_list.extend(indexes.squeeze().tolist())
+
+        encoder.encode_with_indexes(
+            symbols_list, indexes_list, cdf, cdf_lengths, offsets
+        )
+
+        string = encoder.flush()
+        return string
+
+    def decompress(self, strings, shape):
+        assert isinstance(strings, list) and len(strings) == 2
+
+        if next(self.parameters()).device != torch.device("cpu"):
+            warnings.warn(
+                "Inference on GPU is not recommended for the autoregressive "
+                "models (the entropy coder is run sequentially on CPU)."
+            )
+
+        # FIXME: we don't respect the default entropy coder and directly call the
+        # range ANS decoder
+
+        z_hat = self.entropy_bottleneck.decompress(strings[1], shape)
+        params = self.h_s(z_hat)
+
+        s = 4  # scaling factor between z and y
+        kernel_size = 5  # context prediction kernel size
+        padding = (kernel_size - 1) // 2
+
+        y_height = z_hat.size(2) * s
+        y_width = z_hat.size(3) * s
+
+        # initialize y_hat to zeros, and pad it so we can directly work with
+        # sub-tensors of size (N, C, kernel size, kernel_size)
+        y_hat = torch.zeros(
+            (z_hat.size(0), self.M, y_height + 2 * padding, y_width + 2 * padding),
+            device=z_hat.device,
+        )
+
+        for i, y_string in enumerate(strings[0]):
+            self._decompress_ar(
+                y_string,
+                y_hat[i : i + 1],
+                params[i : i + 1],
+                y_height,
+                y_width,
+                kernel_size,
+                padding,
+            )
+
+        y_hat = F.pad(y_hat, (-padding, -padding, -padding, -padding))
+        x_hat = self.g_s(y_hat).clamp_(0, 1)
+        return {"x_hat": x_hat}
+
+    def _decompress_ar(
+        self, y_string, y_hat, params, height, width, kernel_size, padding
+    ):
+        cdf = self.gaussian_conditional.quantized_cdf.tolist()
+        cdf_lengths = self.gaussian_conditional.cdf_length.tolist()
+        offsets = self.gaussian_conditional.offset.tolist()
+
+        decoder = RansDecoder()
+        decoder.set_stream(y_string)
+
+        # Warning: this is slow due to the auto-regressive nature of the
+        # decoding... See more recent publication where they use an
+        # auto-regressive module on chunks of channels for faster decoding...
+        for h in range(height):
+            for w in range(width):
+                # only perform the 5x5 convolution on a cropped tensor
+                # centered in (h, w)
+                y_crop = y_hat[:, :, h : h + kernel_size, w : w + kernel_size]
+                ctx_p = F.conv2d(
+                    y_crop,
+                    self.context_prediction.weight,
+                    bias=self.context_prediction.bias,
+                )
+                # 1x1 conv for the entropy parameters prediction network, so
+                # we only keep the elements in the "center"
+                p = params[:, :, h : h + 1, w : w + 1]
+                gaussian_params = self.entropy_parameters(torch.cat((p, ctx_p), dim=1))
+                scales_hat, means_hat = gaussian_params.chunk(2, 1)
+
+                indexes = self.gaussian_conditional.build_indexes(scales_hat)
+                rv = decoder.decode_stream(
+                    indexes.squeeze().tolist(), cdf, cdf_lengths, offsets
+                )
+                rv = torch.Tensor(rv).reshape(1, -1, 1, 1)
+                rv = self.gaussian_conditional.dequantize(rv, means_hat)
+
+                hp = h + padding
+                wp = w + padding
+                y_hat[:, :, hp : hp + 1, wp : wp + 1] = rv
+
+if __name__ == "__main__":
+    model = LFContext(N=48, angRes=13, n_blocks=1, n_layers=1)
+    # model = JointAutoregressiveHierarchicalPriors(192, 192)
+    # model = Cheng2020Attention(128)
+    input = torch.Tensor(1, 3, 832, 832)
+    # from torchvision import models
+    # model = models.resnet18()
+    print(model)
+    out = model(input)
+    flops, params = get_model_complexity_info(model, ( 3, 832, 832), as_strings=True, print_per_layer_stat=True)
+    print('flops: ', flops, 'params: ', params)
+    # flops, params = profile(model, (input,))
+    # print('flops: ', flops, 'params: ', params)
